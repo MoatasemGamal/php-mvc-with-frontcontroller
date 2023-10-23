4c22aaea2f76b69e7c76cb7a864afdb0a69ce3b1
MoatasemGamal <moatasemco@gmail.com>
2023-10-23 15:02:34 +0300

    create main files and require it in index

---
+++ app/lib/helpers.php
@@ -0,0 +1,10 @@
+<?php
+if (!function_exists("pre")) {
+        function pre($var, $prefix = null)
+        {
+                echo $prefix ? "<h1>$prefix</h1>" : "";
+                echo "<pre dir='ltr'>";
+                var_dump($var);
+                echo "</pre>";
+        }
+}
---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+
+require_once(dirname(__DIR__) . "/app/config.php");
+require_once(APP_PATH . "lib/helpers.php");
+require_once(APP_PATH . "lib/autoload.php");
+
+
