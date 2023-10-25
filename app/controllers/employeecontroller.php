4afa5a565cb60a656a1b11b35bc81338af3239b1
MoatasemGamal <moatasemco@gmail.com>
2023-10-25 10:23:16 +0300

    get employees by model and view the data

--- app/controllers/employeecontroller.php
+++ app/controllers/employeecontroller.php
@@ -2,10 +2,13 @@
 
 namespace MVC\Controllers;
 
+use MVC\Models\EmployeeModel;
+
 class EmployeeController extends AbstractController
 {
     public function defaultAction()
     {
+        $this->_data["employees"] = EmployeeModel::getAll();
         $this->_view();
     }
 }
--- app/views/employee/default.view.php
+++ app/views/employee/default.view.php
@@ -1,1 +1,4 @@
 <h1>Employee</h1>
+<?php
+
+pre($employees);
